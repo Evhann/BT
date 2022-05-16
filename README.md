08598a9b8e899686d8d29deb5fb818c531c62387
Chateauvision <89737965+Chateauvisionn@users.noreply.github.com>
2022-05-16 14:20:00 +0200

    Added --interpret arg

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 Click on this image to see the video:
 
 [![Video](http://img.youtube.com/vi/EJAcFkrdY4c/0.jpg)](http://www.youtube.com/watch?v=EJAcFkrdY4c "BT Installation & Usage (Python DOS)")
+
+## Interpret a file
+To interpret a file use ```--interpret example.txt``` arg or the ```file example.txt``` command.
--- src/main.py
+++ src/main.py
@@ -8,8 +8,14 @@ print(ver)
 if len(sys.argv) > 0:
     if sys.argv[1] == "-nc" or sys.argv[1] == "--no-clear":
         pass
-else:
-    clear()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "-i" or sys.argv[1] == "--interpret":
+            from interpreter import interpret_file
+            interpret_file(sys.argv[2])
+            sys.exit(0)
+        
+    else:
+        clear()
 
 try:
     interpreter() # start command interpreter
