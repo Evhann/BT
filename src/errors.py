852c397db83990ff14b817ba6ad1d2bc5b5be79c
Evhan <89737965+Evhann@users.noreply.github.com>
2023-01-28 14:37:55 +0100

    Update errors.py

--- src/errors.py
+++ src/errors.py
@@ -1,7 +1,7 @@
 # /BT/src/errors.py
 
 def UnknownCommand(command):
-    print(f"Command {command} doesn't exist.")
+    print(f"Command \"{command}\" doesn't exist.")
 
 class NotEnoughPermissions(Exception):
     pass
